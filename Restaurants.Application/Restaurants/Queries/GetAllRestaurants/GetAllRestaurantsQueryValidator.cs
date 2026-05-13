453c58a3d9b7cf5b5d829887d579408706bccf10
AbdallahEld <abdallaheldesoky301@gmail.com>
2026-05-13 16:46:29 +0300

    Pagination validation

---
+++ Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsQueryValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Application
+{
+    public class GetAllRestaurantsQueryValidator : AbstractValidator<GetAllRestaurantsQuery>
+    {
+        private int[] allowPageSizes = [5, 10, 15, 30];
+        public GetAllRestaurantsQueryValidator()
+        {
+            RuleFor(x => x.PageNumber)
+                .GreaterThan(0)
+                .WithMessage("Page number must be greater than 0.");
+
+            RuleFor(x => x.PageSize)
+                .Must(pageSize => allowPageSizes.Contains(pageSize))
+                .WithMessage($"Page size must be one of the following values: {string.Join(", ", allowPageSizes)}.");
+        }
+    }
+}
