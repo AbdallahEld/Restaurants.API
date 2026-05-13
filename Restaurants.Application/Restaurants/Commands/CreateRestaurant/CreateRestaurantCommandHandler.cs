13587b7ed91cc4c65d92bde4202e36f7e6b4ea83
AbdallahEld <abdallaheldesoky301@gmail.com>
2026-05-13 13:25:08 +0300

    User resources

--- Restaurants.Application/Restaurants/Commands/CreateRestaurant/CreateRestaurantCommandHandler.cs
+++ Restaurants.Application/Restaurants/Commands/CreateRestaurant/CreateRestaurantCommandHandler.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using Restaurants.Application.User;
 using Restaurants.Domain;
 
 namespace Restaurants.Application
@@ -8,12 +9,20 @@ namespace Restaurants.Application
     public class CreateRestaurantCommandHandler(
         ILogger<CreateRestaurantCommandHandler> logger,
         IMapper mapper,
-        IRestaurantRepository restaurantRepository) : IRequestHandler<CreateRestaurantCommand, int>
+        IRestaurantRepository restaurantRepository,
+        IUserContext userContext) : IRequestHandler<CreateRestaurantCommand, int>
     {
         public async Task<int> Handle(CreateRestaurantCommand request, CancellationToken cancellationToken)
         {
-            logger.LogInformation("Create Restaurant {@Restaurant}", request);
+            var currentUser =  userContext.GetCurrentUser();
+
+            logger.LogInformation("{UserEmail} {UserId} is creating a new restaurant {@Restaurant}",
+                currentUser.Email,
+                currentUser.Id,
+                request);
+
             var restaurant = mapper.Map<Restaurant>(request);
+            restaurant.OwnerId = currentUser.Id;
 
             var id = await restaurantRepository.CreateAsync(restaurant);
             return id;
--- Restaurants.Domain/Entities/User.cs
+++ Restaurants.Domain/Entities/User.cs
@@ -6,5 +6,7 @@ namespace Restaurants.Domain
     {
         public DateOnly? DateOfBirth { get; set; }
         public string? Nationality { get; set; }
+
+        public List<Restaurant> OwnedRestaurants { get; set; } = [];
     }
 }
